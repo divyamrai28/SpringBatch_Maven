478bf24a77c989b3420ef488df49f3a2d6b3133d
Divyam Rai <divyam.referral@gmail.com>
2023-01-26 10:39:08 +0530

    first commit

---
+++ src/main/java/rai/in/SpringBatch_Maven/SpringBatchMavenApplication.java
@@ -0,0 +1,13 @@
+package rai.in.SpringBatch_Maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rai/in/SpringBatch_Maven/SpringBatchMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package rai.in.SpringBatch_Maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
